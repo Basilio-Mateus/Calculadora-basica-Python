8df8b7d7c24ddcba5d684f96d9c5937a85c40ffa
Mateus Basilio <mateusbasilioemilio@gmail.com>
2025-04-03 14:29:55 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um número:'))
+n2 = int(input('Digite outro número:'))
+r = n1+n2
+#print('A soma entre',n1,'e',n2,'vale',r)
+print(' a soma de {} e {} vale {}'.format(n1,n2,r))
